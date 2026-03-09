82ec65f47c1d075ea40984e1f02e53c12421ad7f
PML9462 <kamlesh@paulmerchants.net>
2026-03-09 17:11:57 +0530

    updated

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -7,28 +7,55 @@ const { accountService } = require("../services/index");
 
 const addBankAccount = async (req, res) => {
 
-    req.body.customerId = req.customer.customerId; // Assuming auth middleware sets req.user
+    req.body.customerId = req.customer.customerId;
 
-    const bankAccount = await accountService.addBankAccount(req.body);
-    return res.status(httpStatus.status.CREATED).send(bankAccount);
+    const result = await accountService.addBankAccount(req.body);
+
+    return res.status(httpStatus.status.CREATED).json({
+        success: true,
+        status: httpStatus.status.CREATED,
+        message: "Account added successfully",
+        data: result
+    });
 
 }
 const getBankAccounts = async (req, res) => {
 
-    const bankAccounts = await accountService.getBankAccounts(req.query);
-    return res.status(httpStatus.status.OK).send(bankAccounts);
+    const result = await accountService.getBankAccounts(req.query);
+
+    return res.status(httpStatus.status.OK).json({
+        success: true,
+        status: httpStatus.status.OK,
+        message: "Account fetched successfully",
+        data: result
+    });
+
 }
 
 const addUpiAccount = async (req, res) => {
     req.body.customerId = req.customer.customerId; // Assuming auth middleware sets req.user
 
-    const upiAccount = await accountService.addUpiAccount(req.body);
-    return res.status(httpStatus.status.CREATED).send(upiAccount);
+    const result = await accountService.addUpiAccount(req.body);
+
+    return res.status(httpStatus.status.CREATED).json({
+        success: true,
+        status: httpStatus.status.CREATED,
+        message: "UPI account added successfully",
+        data: result
+    });
+
 }
 const getUpiAccounts = async (req, res) => {
 
-    const upiAccounts = await accountService.getUpiAccounts(req.query);
-    return res.status(httpStatus.status.OK).send(upiAccounts);
+    const result = await accountService.getUpiAccounts(req.query);
+
+    return res.status(httpStatus.status.OK).json({
+        success: true,
+        status: httpStatus.status.OK,
+        message: "UPI accounts fetched successfully",
+        data: result
+    });
+
 }
 
 module.exports = {
